cd96df7f5efbd7b538977fad43fb2c4e80d71459
Мария Алексеева <12ficus12@gmail.com>
2024-03-16 15:28:09 +0400

    Initial commit

---
+++ create_word.py
@@ -0,0 +1,49 @@
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from models import create_tables, Word
+
+
+
+def create_db(engine):
+    word = (
+        ('hello', 'привет'),
+        ('goodbye', 'до свидания'),
+        ('yes', 'да'),
+        ('no', 'нет'),
+        ('ok', 'хорошо'),
+        ('Sun', 'солнце'),
+        ('Moon', 'луна'),
+        ('Star', 'звезда'),
+        ('Tree', 'дерево'),
+        ('House', 'дом'),
+        ('Car', 'машина'),
+        ('Book', 'книга'),
+        ('Phone', 'телефон'),
+        ('Computer', 'компьютер'),
+        ('Keyboard', 'клавиатура'),
+        ('Mouse', 'мышь'),
+        ('Green', 'зеленый'),
+        ('White', 'белый'),
+        ('Red', 'красный'),
+        ('Yellow', 'желтый'),
+        ('Blue', 'синий')
+    )
+    
+    create_tables(engine)
+    for i in word:
+        
+        session.add(Word(word=i[0], translate=i[1]))
+        
+    session.commit()
+
+
+    
+
+DNS = 'postgresql://postgres:25022004HERSOSI1725@localhost:5432/tgbot'
+engine = sqlalchemy.create_engine(DNS)
+Session = sessionmaker(bind=engine)
+session = Session()
+create_db(engine)
+
+session.close()
+
---
+++ models.py
@@ -0,0 +1,38 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "user"
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=50), nullable=False)
+
+    def __str__(self):
+        return self.name
+    
+class UserWord(Base):
+    __tablename__ = "user_word"
+    id = sq.Column(sq.Integer, primary_key=True)
+    word = sq.Column(sq.String(length=50), unique=True)
+    translate = sq.Column(sq.String(length=50), nullable=False)
+    user_id = sq.Column(sq.Integer, sq.ForeignKey("user.id"), nullable=False)
+
+    user =relationship("User",  backref="user_words")
+
+    def __str__(self):
+        return self.word
+    
+class Word(Base):
+    __tablename__ = "word"
+    id = sq.Column(sq.Integer, primary_key=True)
+    word = sq.Column(sq.String(length=50), unique=True)
+    translate = sq.Column(sq.String(length=50), nullable=False)
+
+    def __str__(self):
+        return self.word
+    
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
